16335694dd2de15c3c830b91e6cb995f679f1c28
sarah-frk <franke.sarah01@gmail.com>
2020-12-21 09:13:20 +0100

    Test
    Das ist ein Test

--- Murmelbahn/js/FactoryJAVASCRIPT.js
+++ Murmelbahn/js/FactoryJAVASCRIPT.js
@@ -4,6 +4,7 @@ Homeworks.aufgabe = 8;
 // Benno Stäbler: kopiert vom 02-mouse Beispiel, erweitert um komplexe Bodies und in die bekannte Struktur gebracht
 // Hier ist alles mit Classes codiert
 // Hier ist alles Gut!!
+// Test
 let engine
 let polySynth
 let mouseConstraint
